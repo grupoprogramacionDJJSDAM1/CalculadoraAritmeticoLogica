cb6774810973b0702c7463e6e7a2230a1b8057bf
grupoprogramacionDJJSDAM1 <94907412+grupoprogramacionDJJSDAM1@users.noreply.github.com>
2021-12-14 16:12:58 +0100

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,4 +1,6 @@
 
+
+import java.io.BufferedReader;
 /* Enunciado: Calculadora de operaciones modularizada.
  * Fecha: 10/11/21
  * Autor: Jaime Fernando Martín Gil, Jorge Parra López, David Mateos Lorenzo y Saul Fernández García.
@@ -10,6 +12,7 @@
  *  
  */
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -19,33 +22,33 @@ public class Calculadora {
 	private static int numero2; //numero2 con el que quiere operar el usuario
 	private static boolean valorlogico1; //valor logico1 (true o false) con el que quiere operar el usuario
 	private static boolean valorlogico2;//valor logico2 (true o false) con el que quiere operar el usuario
-	static Scanner teclado = new Scanner(System.in);
+	//static Scanner teclado = new Scanner(System.in);
+	private static BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 	private static boolean salirBucle=false;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		menuPrincipal();
 	}
 
 	//PEDIR DATOS ARITMÉTICOS
 	
-	public static void pedirDatos() { // Metodo que pide datos al usuario
+	public static void pedirDatos() throws NumberFormatException, IOException { // Metodo que pide datos al usuario
 
 		System.out.println("Introduce el numero1 con el que quiere operar: ");
-		numero1 = teclado.nextInt(); // recoge el numero1
+		numero1 = Integer.parseInt(br.readLine()); // recoge el numero1
 		System.out.println("Introduce el numero2 con el que quiere operar : ");
-		numero2 = teclado.nextInt(); // recoge el numero2
+		numero2 = Integer.parseInt(br.readLine()); // recoge el numero2
 	
 	}
 
 	//PEDIR DATOS LOGICOS
 	
-	public static void pedirDatosLogicos() {
-
+	public static void pedirDatosLogicos() throws NumberFormatException, IOException  {
 
 		System.out.println("Introduce true o false ");
 		System.out.println("Introduce el primer valor con el que quieres operar:");
-		valorlogico1 = teclado.nextBoolean(); // recoge el primer valor 
+		valorlogico1 = Boolean.parseBoolean(br.readLine());// recoge el primer valor 
 		System.out.println("Introduce el segundo valor con el que quieres operar:");
-		valorlogico2 = teclado.nextBoolean(); // recoge el segundo valor
+		valorlogico2 = Boolean.parseBoolean(br.readLine()); // recoge el segundo valor
 
 	}
 	//PINTAR MENÚ PRINCIPAL
@@ -95,12 +98,12 @@ public class Calculadora {
 	
 	//MENÚ PRINCIPAL
 	
-	public static void menuPrincipal() {
+	public static void menuPrincipal() throws IOException {
 		
 	do {
 		try {
 		pintarMenuPrincipal();
-		opcion = teclado.nextInt(); // lee la opcion del usuario
+		opcion = Integer.parseInt(br.readLine()); // lee la opcion del usuario
 		
 		switch (opcion) {
 		case 1:
@@ -114,13 +117,13 @@ public class Calculadora {
 		case 3: 
 			System.out.println("Has elegido salir");
 			salirBucle=true;
-			teclado.next(); //Para que no me vuelva a mostrar el menu (error con scanner)
+			br.readLine();//Para que no vuelva a mostrar el menu o se meta en bucle infinito
 			break;
 			default: System.out.println("numeros del 1 al 3!!");
 		}
-	}catch(InputMismatchException e) {
+	}catch(NumberFormatException e) {
 		System.out.println("Error!! solo se admiten numeros"); 
-		teclado.next();//para evitar que entre en un bucle infinito
+		
 	}
 	
 	}while (salirBucle == false);
@@ -129,7 +132,7 @@ public class Calculadora {
 	
 //MENÚ OPERACIONES LÓGICAS	
 	
-	public static void menuLogicas() {
+	public static void menuLogicas() throws IOException {
 		boolean salirBucle1=false;
 		int opcion;
 		do {
@@ -137,16 +140,15 @@ public class Calculadora {
 				
 		pintarMenuLogicas();
 
-		opcion = teclado.nextInt(); // lee la opcion del usuario
+		opcion = Integer.parseInt(br.readLine()); // lee la opcion del usuario
 		switch (opcion) {
 		case 1:
 			System.out.println("Has elegido la operacion logica and. ");
 			try {
 			pedirDatosLogicos(); // llamada al metodo que pide datos al usuario
 		System.out.println("El resultado es " + and(valorlogico1, valorlogico2)); // realiza la operacion AND.
-			}catch(InputMismatchException e){
+			}catch(NumberFormatException e){
 				System.out.println("Error!! solo true o false !!");
-				teclado.next();
 			}
 		menuLogicas();
 			break;
@@ -156,9 +158,8 @@ public class Calculadora {
 			try { 
 			pedirDatosLogicos();
 			System.out.println("El resultado es " + or(valorlogico1, valorlogico2)); // realiza la operacion OR.
-			}catch(InputMismatchException e){
+			}catch(NumberFormatException e){
 				System.out.println("Error!! solo true o false !!");
-				teclado.next();
 			}
 			menuLogicas();			
 			break;
@@ -167,9 +168,8 @@ public class Calculadora {
 			try {
 			pedirDatosLogicos();
 			System.out.println("El resultado es " + not(valorlogico1)); // realiza la operacion NOT.
-			}catch(InputMismatchException e){
+			}catch(NumberFormatException e){
 				System.out.println("Error!! solo true o false !!");
-				teclado.next();
 			}
 			menuLogicas();
 			break;
@@ -178,9 +178,8 @@ public class Calculadora {
 			try {
 			pedirDatosLogicos();
 			System.out.println("El resultado es " + xor(valorlogico1, valorlogico2));// realiza la operacion XOR.
-			}catch(InputMismatchException e){
+			}catch(NumberFormatException e){
 				System.out.println("Error!! solo true o false !!");
-				teclado.next();
 			}
 			menuLogicas();
 			break;
@@ -194,14 +193,13 @@ public class Calculadora {
 			System.out.println("Se ha salido del programa correctamente");
 			salirBucle1 = true; // salirBucle a true para que salga del bucle
 			salirBucle=true;
-			teclado.next();
+			br.readLine();//Para que no vuelva a mostrar el menu o se meta en bucle infinito
 			break;
 		default:
             System.out.println("Solo números entre 1 y 6");
 		}
 		}catch(NumberFormatException  e ) {
 			System.out.println("Error!! solo se admiten numeros !!");
-			teclado.next();
 		}
 			
 	}while (salirBucle1 == false);
@@ -209,13 +207,13 @@ public class Calculadora {
 
 	//MENÚ OPERACIONES ARITMETICAS
 
-	public static void menuAritmeticas() { // Metodo menu() que llama a pintarOpcionesMenu, recoge la opcion y realiza la llamada a las operaciones segun la eleccion del usuario 
+	public static void menuAritmeticas() throws NumberFormatException, IOException { // Metodo menu() que llama a pintarOpcionesMenu, recoge la opcion y realiza la llamada a las operaciones segun la eleccion del usuario 
 		boolean salirBucle2=false;
 		int opcion;
 		do {
 			try {
 			pintarOpcionesAritmeticas();
-			opcion = teclado.nextInt(); // lee la opcion del usuario
+			opcion = Integer.parseInt(br.readLine()); // lee la opcion del usuario
 			switch (opcion) {
 			case 1:
 				System.out.println("Has elegido la suma. ");
@@ -255,14 +253,13 @@ public class Calculadora {
 				System.out.println("Se ha salido del programa correctamente");
 				salirBucle2 = true; // salirBucle a true para que salga del bucle
 				salirBucle=true; //Salgo del bucle del menu principal
-				teclado.next(); //para que no haga un bucle infinito o no vuelva a repetir el menú
+				br.readLine();
 				break;
 			default:
                 System.out.println("Solo números entre 1 y 6");
 			}
-			}catch(InputMismatchException e ) {
+			}catch(NumberFormatException e ) {
 				System.out.println("Error!! solo se admiten numeros!!");
-				teclado.next();
 			
 			}
 		} while (salirBucle2 == false);
